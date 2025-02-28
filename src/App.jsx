00914250fcefb71b3a1307d0fcb944eb56b3f7f2
charlzspice <charlzspice@gmail.com>
2025-03-01 01:55:20 +0300

    rename images for caurossel

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-// eslint-disable-next-line no-unused-vars
 import React from "react";
 import Carossel from "./Components/Carossel.jsx"; // Correct import
 
 // Import local images
-import img1 from "./assets/image1.jpg";
-import img2 from "./assets/image2.jpg";
-import img3 from "./assets/image3.jpg";
-import img4 from "./assets/image4.jpg";
+import img1 from "./assets/c1.jpg";
+import img2 from "./assets/c2.jpg";
+import img3 from "./assets/c3.jpg";
+import img4 from "./assets/c4.jpg";
 
 function App() {
   const images = [img1, img2, img3, img4];
