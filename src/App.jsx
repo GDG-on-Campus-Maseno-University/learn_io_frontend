e107dbb561e28954279aaab4723e90b9df3fe1d0
Clifford537 <cliffordmukosh@gmail.com>
2025-03-14 19:46:01 -0400

    updates on project stracture

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 import React from "react";
-import Carossel from "./Components/Carossel.jsx"; 
+import Carosalepage from "./carossel/CarosselPage"
 
-// Import local images
-import img1 from "./assets/c1.jpg";
-import img2 from "./assets/c2.jpg";
-import img3 from "./assets/c3.jpg";
-import img4 from "./assets/c4.jpg";
 
-function App() {
-  const images = [img1, img2, img3, img4];
-
-  return (
-    <div>
-      <h1>Image Slider</h1>
-      <Carossel images={images} speed={50} />
-    </div>
-  );
+function App(){
+  return(
+    <>
+     <Carosalepage/>
+    </>
+  )
 }
 
 export default App;
---
+++ src/carossel/CarosselPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Carossel from "./Carossel.jsx"; 
+
+// Import local images
+import img1 from "../assets/c1.jpg";
+import img2 from "../assets/c2.jpg";
+import img3 from "../assets/c3.jpg";
+import img4 from "../assets/c4.jpg";
+
+function Carosalepage() {
+  const images = [img1, img2, img3, img4];
+
+  return (
+    <div>
+      <h1>Image Slider</h1>
+      <Carossel images={images} speed={50} />
+    </div>
+  );
+}
+
+export default Carosalepage;
