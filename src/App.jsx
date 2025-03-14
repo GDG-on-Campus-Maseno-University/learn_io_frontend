fee80d0502553ee1389f6ba64d75bf2e9d3f71cb
Benaiah Galavu <bentheaya@gmail.com>
2025-03-15 00:34:57 +0300

    Lessons Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Carosalepage from "./carossel/CarosselPage"
 import Home from "./Components/Home";
+import Lesson from "./lessonpages/Lesson";
 
 
 function App(){
   return(
     <>
-     <Home/>
+      <Home />
     </>
   )
 }
