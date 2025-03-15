e7db58906386109628e66295c4c8fc30f1744fe6
Clifford537 <cliffordmukosh@gmail.com>
2025-03-14 22:53:21 -0400

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Carosalepage from "./carossel/CarosselPage"
+import Home from "./Components/Home";
 
 
 function App(){
   return(
     <>
-     <Carosalepage/>
+     <Home/>
     </>
   )
 }
