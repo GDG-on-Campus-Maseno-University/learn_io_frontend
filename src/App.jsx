a29a4408c05eee97b09ddb4f2aa2262da2bb5a20
charlzspice <charlzspice@gmail.com>
2025-03-29 00:50:41 +0300

    add a profile page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 import Profile from "./profile/Profile";
-import Carosalepage from "./carossel/CarosselPage"
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import InstructorPage from "./instructorpages/InstructorPage";
-
-function App(){
-  return(
+function App() {
+  return (
     <>
-    <Profile/>
+      <Profile />
     </>
-  )
+  );
 }
 
 export default App;
