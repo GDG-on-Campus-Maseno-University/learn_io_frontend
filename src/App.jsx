b45898fafb61e1ca34dd053aff02821f8c575f72
KageniLinah <linahkageni3@gmail.com>
2025-03-29 01:13:33 +0300

    added Register form

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-import Profile from "./profile/Profile";
-import Carosalepage from "./carossel/CarosselPage"
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import InstructorPage from "./instructorpages/InstructorPage";
+import Register from "./authentication/Register";
 
 function App(){
   return(
     <>
-    <Profile/>
+    <Register/>
     </>
   )
 }
