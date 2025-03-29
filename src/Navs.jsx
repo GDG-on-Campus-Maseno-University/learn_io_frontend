d61ebdd6d5ad71ab4ddf632b008bda128c10c492
Clifford537 <cliffordmukosh@gmail.com>
2025-03-29 04:13:33 -0400

    updates

--- src/Navs.jsx
+++ src/Navs.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./authentication/Login";
-import Home from "./homepage/HomePage";
 import Articles from "./articlepages/Articles"
 import Home from "./homepage/Home";
 import Lesson from "./lessonpages/Lesson";
-
-
+import Register from "./authentication/Register";
+import LearningTutorial from "./coursepages/LearningPage";
+import Profile from "./profile/Profile";
+import InstructorPage from "./instructorpages/InstructorPage";
+import Find from "./pastpaperpages/findpaper/FindPastpaper";
 function Navs(){
 
     return(
@@ -15,10 +17,14 @@ function Navs(){
 
                 <Route path="/"  element={<Home/>}/>
                 <Route path="/login"  element={<Login/>}/>
-                <Route path="/articles" element={<Articles/>}
-                <Route   path="/"  element={<Home/>}/>
+                <Route path="/articles" element={<Articles/>}/>
                 <Route  path="/login"  element={<Login/>}/>
                 <Route  path="/lesson"  element={<Lesson/>}/>
+                <Route path="/register"  element={<Register/>}/>
+                <Route path="/course"  element={<LearningTutorial/>}/>
+                <Route path="/userprofile"  element={<Profile/>}/>
+                <Route path="/instr"  element={<InstructorPage/>}/>
+                <Route path="/findpp"  element={<Find/>}/>
             </Routes>
         </Router>
     )
