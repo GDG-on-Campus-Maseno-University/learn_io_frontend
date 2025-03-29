c8da3f6719ffda43b54663186455aa9e6c8c73f6
Newton <newton.b@m365connect.com>
2025-03-29 04:11:22 +0300

    Login

--- src/Navs.jsx
+++ src/Navs.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./authentication/Login";
-import Home from "./homepage/HomePage";
+// import Home from "./homepage/HomePage";
 import Articles from "./articlepages/Articles"
 import Home from "./homepage/Home";
 import Lesson from "./lessonpages/Lesson";
+// import ForgotPassword from "./authentication/ForgotPassword.jsx";
+import ResetPassword from "./authentication/ResetPassword.jsx";
 
 
 function Navs(){
@@ -15,10 +17,10 @@ function Navs(){
 
                 <Route path="/"  element={<Home/>}/>
                 <Route path="/login"  element={<Login/>}/>
-                <Route path="/articles" element={<Articles/>}
+                <Route path="/articles" element={<Articles/>}/>
                 <Route   path="/"  element={<Home/>}/>
-                <Route  path="/login"  element={<Login/>}/>
                 <Route  path="/lesson"  element={<Lesson/>}/>
+                <Route path="/reset-password" element={<ResetPassword />} />
             </Routes>
         </Router>
     )
--- src/homepage/Home.jsx
+++ src/homepage/Home.jsx
@@ -8,7 +8,7 @@ function Home(){
         {/* <Carossel/> */}
         <Footer/>
         </>
-    )
+    );
 }
 
 export default Home;
